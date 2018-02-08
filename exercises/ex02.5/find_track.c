405db45fb49055cdeddcdb81aeab80e4cb74ec6a
Uma Desai <uma.desai@students.olin.edu>
2018-02-08 00:54:13 -0500

    updated find_track_regex()

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -1,12 +1,14 @@
 /* Example code for Exercises in C.
 
 Modified version of an example from Chapter 2.5 of Head First C.
+Also contains code from http://www.peope.net/old/regex.html.
 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -37,7 +39,24 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+  int i;
+  regex_t regex;
+  int reti;
+
+  reti = regcomp(&regex, pattern, 0);
+  if (reti){
+    fprintf(stderr, "Could not compile regex\n");
+    exit(1);
+  }
+  else{
+    for (i=0; i<NUM_TRACKS; i++) {
+        reti = regexec(&regex, tracks[i], 0, NULL, 0);
+        if( !reti ){
+                printf("Match! Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+  }
+  regfree(&regex);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -58,8 +77,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
