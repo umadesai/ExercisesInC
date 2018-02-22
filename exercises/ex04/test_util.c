fb8b5d8793f55de3b7fac8e440496de74a367108
Uma Desai <uma.desai@students.olin.edu>
2018-02-21 22:30:51 -0500

    updated test_util.c

---
+++ exercises/ex04/test_util.c
@@ -0,0 +1,38 @@
+/* Test code for util.c in Software Systems class at Olin College.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "trout/util.h"
+#include "minunit.h"
+
+int tests_run = 0;
+
+static char *test1() {
+    char *res = icmpcode_v4(0);
+    char *message = "test1 failed: icmpcode_v4(0) should return 'network unreachable'";
+    mu_assert(message, res == "network unreachable");
+    return NULL;
+}
+
+static char * all_tests() {
+    mu_run_test(test1);
+    // mu_run_test(test2);
+    // mu_run_test(test3);
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != NULL) {
+        printf("%s\n", result);
+    } else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
