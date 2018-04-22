9e7b0dc4022b37465ca73ddff3b362357d609928
Uma Desai <uma.desai@students.olin.edu>
2018-04-22 09:34:28 -0400

    update exercise 11

---
+++ exercises/ex11/advice_client.c
@@ -0,0 +1,63 @@
+/* This is a client that connects to advice_server and prints a reply.
+Based on code from Head First C.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+int open_socket(char *host, char *port)
+{
+    struct addrinfo *res;
+    struct addrinfo hints;
+    int d_sock, c;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if (getaddrinfo(host, port, &hints, &res) == -1)
+        error("Can't resolve the address");
+    if (( d_sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
+        error("Can't open socket");
+
+    c = connect(d_sock, res->ai_addr, res->ai_addrlen);
+    freeaddrinfo(res);
+    if (c == -1)
+        error("Can't connect to the socket");
+
+    return d_sock;
+}
+
+int main(int argc, char *argv[])
+{
+    int d_sock, bytes_received;
+    char buf[255], rec[256];
+
+    /* connect to server */
+    d_sock = open_socket("localhost", "30000");
+
+    /* display page on stdout in 255 byte chunks */
+    bytes_received = recv(d_sock, rec, 255, 0);
+    while (bytes_received) {
+        if (bytes_received == EOF)
+            error("can't read from server");
+        rec[bytes_received] = '\0';
+        printf("%s", rec);
+        bytes_received = recv(d_sock, rec, 255, 0);
+    }
+    close(d_sock);
+
+    return 0;
+}
